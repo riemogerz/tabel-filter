2fc4d2b13e6231f69d6ddfa90009fb01c68fbacc
riemogerz <rizqanhukum@gmail.com>
2016-05-16 02:18:03 +0800

    Create riemogerz_tablefilter2.js

---
+++ riemogerz_tablefilter2.js
@@ -0,0 +1,33 @@
+// <![CDATA[
+      var filtersConfig = {
+    base_path: '',
+    col_1: 'select',
+    col_2: 'select',
+    col_3: 'select',
+    alternate_rows: true,
+    rows_counter: true,
+    btn_reset: true,
+    loader: true,
+    mark_active_columns: true,
+    highlight_keywords: true,
+    col_number_format: [
+        null,
+        null,
+        'US',
+        null,
+    ],
+    col_widths: [
+        '100px',
+        '50px',
+        '100px',
+        '70px',
+    ],
+    extensions: [{
+            name: 'sort',
+            images_path: 'https://koalyptus.github.io/TableFilter/tablefilter/style/themes/'
+        }]
+};
+var tf = new TableFilter('riemogerz', filtersConfig);
+tf.init();
+      //# sourceURL=pen.js
+// ]]>
